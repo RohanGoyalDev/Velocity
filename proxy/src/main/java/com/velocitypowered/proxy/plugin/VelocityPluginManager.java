12d8c0a52f878265e3e9cdf8c132740cf3de350e
Clément Raynaud <30211659+carlodrift@users.noreply.github.com>
2024-12-25 00:04:42 +0100

    fix: remove unused import

--- proxy/src/main/java/com/velocitypowered/proxy/plugin/VelocityPluginManager.java
+++ proxy/src/main/java/com/velocitypowered/proxy/plugin/VelocityPluginManager.java
@@ -45,7 +45,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
